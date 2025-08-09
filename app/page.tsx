cb6f41317a221f2a0418cc4bacf21e6c82d8908e
tommy <zombietommie@gmail.com>
2025-08-08 19:24:23 -0500

    added image and adjusted the text/img center

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
       <main>
-          <div className="header">
+          <div className="header justify-items-center">
               <h1>Paige Seafood</h1>
           </div>
-          <div className="body">
+          <div className="body justify-items-center">
               <h3>Body of Paige Seafood</h3>
+              <Image src="/paigeseafood_store_image.jpg"
+                     alt={"The storefront of the business Paige Seafood"}
+                     width={600}
+                     height={400}
+              />
           </div>
-          <div className="footer">
+          <div className="footer justify-items-center   ">
               <h3>Footer of Paige Seafood</h3>
           </div>
       </main>
